118d2abaa068d161273104a9165d335f915b2889
Igor S Fugiwara <igorsfugiwara@outlook.com>
2022-06-16 01:32:20 -0300

    continuando amanha

--- src/pages/alerta-de-inundacao.tsx
+++ src/pages/alerta-de-inundacao.tsx
@@ -7,7 +7,7 @@ return (
     <div>
     <Caminho atual="Alerta de Inundação" />
     <Titulo>Alerta de Inundação</Titulo>
-    <div className="mapouter"><div className="gmap_canvas"><iframe width={1000} height={300} id="gmap_canvas" src="https://maps.google.com/maps?q=capao%20redondo&t=&z=17&ie=UTF8&iwloc=&output=embed" frameBorder={0} scrolling="no" marginHeight={0} marginWidth={0} /><a href="https://2piratebay.org" /><br /><style dangerouslySetInnerHTML={{__html: ".mapouter{position:relative;text-align:right;height:300px;width:1000px;}" }} /><a href="https://www.embedgooglemap.net">embedgooglemap.net</a><style dangerouslySetInnerHTML={{__html: ".gmap_canvas {overflow:hidden;background:none!important;height:300px;width:1000px;}" }} /></div></div>
+    <div className="mapouter"><div className="gmap_canvas"><iframe width={1200} height={300} id="gmap_canvas" src="https://maps.google.com/maps?q=capao%20redondo&t=&z=17&ie=UTF8&iwloc=&output=embed" frameBorder={0} scrolling="no" marginHeight={0} marginWidth={0} /><a href="https://2piratebay.org" /><br /><style dangerouslySetInnerHTML={{__html: ".mapouter{position:relative;text-align:right;height:300px;width:1200px;}" }} /><a href="https://www.embedgooglemap.net">embedgooglemap.net</a><style dangerouslySetInnerHTML={{__html: ".gmap_canvas {overflow:hidden;background:none!important;height:300px;width:1200px;}" }} /></div></div>
     </div>
 
   )
